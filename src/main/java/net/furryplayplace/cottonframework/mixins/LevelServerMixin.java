83429b01919fe1088c87e74b069288fcffb23635
NebraskyTheWolf <farfy.dev@gmail.com>
2024-12-20 04:47:20 +0100

    Remove unused game event injection from LevelServerMixin.
    The `onEmitGameEvent` method and related imports were removed due to redundancy and lack of usage. This cleanup reduces unnecessary code and potential maintenance overhead without impacting core functionality.

--- src/main/java/net/furryplayplace/cottonframework/mixins/LevelServerMixin.java
+++ src/main/java/net/furryplayplace/cottonframework/mixins/LevelServerMixin.java
@@ -15,14 +15,9 @@ Last Modified : 19.12.2024
 package net.furryplayplace.cottonframework.mixins;
 
 import net.furryplayplace.cottonframework.api.CottonAPI;
-import net.furryplayplace.cottonframework.api.Location;
 import net.furryplayplace.cottonframework.api.events.weather.ThunderChangeEvent;
 import net.furryplayplace.cottonframework.api.events.weather.WeatherChangeEvent;
-import net.furryplayplace.cottonframework.api.events.world.GenericGameEvent;
-import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.event.GameEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -33,19 +28,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class LevelServerMixin {
 
     @Shadow public abstract ServerWorld toServerWorld();
-
-    @Inject(method = "emitGameEvent", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/event/listener/GameEventDispatchManager;dispatch(Lnet/minecraft/registry/entry/RegistryEntry;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/world/event/GameEvent$Emitter;)V"))
-    public void onEmitGameEvent(RegistryEntry<GameEvent> event, Vec3d emitterPos, GameEvent.Emitter emitter, CallbackInfo ci) {
-        GameEvent gameEvent = event.value();
-
-        CottonAPI.get().pluginManager().getEventBus().post(new GenericGameEvent(
-                gameEvent,
-                new Location(emitter.sourceEntity().getWorld(), emitterPos.x, emitterPos.y, emitterPos.z),
-                emitter.sourceEntity(),
-                gameEvent.notificationRadius(),
-                false
-        ));
-    }
 
     @Inject(
             method = "setWeather",
