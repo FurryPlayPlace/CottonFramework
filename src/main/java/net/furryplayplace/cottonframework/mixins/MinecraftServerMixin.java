fdf9c06a87f0a3a38081610ae38230f56a991a79
NebraskyTheWolf <farfy.dev@gmail.com>
2024-12-20 04:43:36 +0100

    Remove unused imports and redundant world initialization logic.
    This cleanup removes unnecessary imports and a redirect related to world initialization. The redundant `WorldInitEvent` handling has also been deleted to streamline the `createWorlds` method. These changes simplify the code and improve maintainability.

--- src/main/java/net/furryplayplace/cottonframework/mixins/MinecraftServerMixin.java
+++ src/main/java/net/furryplayplace/cottonframework/mixins/MinecraftServerMixin.java
@@ -14,26 +14,15 @@ Last Modified : 19.12.2024
 
 package net.furryplayplace.cottonframework.mixins;
 
-import net.furryplayplace.cottonframework.api.CottonAPI;
 import net.furryplayplace.cottonframework.api.events.cotton.CottonPluginShutdown;
 import net.furryplayplace.cottonframework.CottonFramework;
-import net.furryplayplace.cottonframework.api.events.world.WorldInitEvent;
 import net.furryplayplace.cottonframework.api.events.world.WorldLoadEvent;
-import net.minecraft.registry.RegistryKey;
 import net.minecraft.server.*;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.random.RandomSequencesState;
-import net.minecraft.world.dimension.DimensionOptions;
-import net.minecraft.world.level.ServerWorldProperties;
-import net.minecraft.world.level.storage.LevelStorage;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.List;
-import java.util.concurrent.Executor;
 
 @Mixin(MinecraftServer.class)
 public abstract class MinecraftServerMixin {
@@ -43,12 +32,6 @@ public abstract class MinecraftServerMixin {
     public void stopServer(CallbackInfo ci) {
         CottonFramework.getInstance().getApi().pluginManager()
                 .getEventBus().post(new CottonPluginShutdown());
-    }
-
-    @Redirect(method = "createWorlds", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;<init>(Lnet/minecraft/server/MinecraftServer;Ljava/util/concurrent/Executor;Lnet/minecraft/world/level/storage/LevelStorage$Session;Lnet/minecraft/world/level/ServerWorldProperties;Lnet/minecraft/registry/RegistryKey;Lnet/minecraft/world/dimension/DimensionOptions;Lnet/minecraft/server/WorldGenerationProgressListener;ZJLjava/util/List;ZLnet/minecraft/util/math/random/RandomSequencesState;)V"))
-    private void redirectServerLevelConstructor(ServerWorld instance, MinecraftServer server, Executor workerExecutor, LevelStorage.Session session, ServerWorldProperties properties, RegistryKey worldKey, DimensionOptions dimensionOptions, WorldGenerationProgressListener worldGenerationProgressListener, boolean debugWorld, long seed, List spawners, boolean shouldTickTime, RandomSequencesState randomSequencesState) {
-        WorldInitEvent worldInitEvent = new WorldInitEvent(instance);
-        CottonAPI.get().pluginManager().getEventBus().post(worldInitEvent);
     }
 
     @Inject(method = "prepareStartRegion", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;getOverworld()Lnet/minecraft/server/world/ServerWorld;"))
